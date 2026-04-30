15dbcc8718dc6a1bbf1086e055f6f0daeaace861
Gopal Krishna <goplakrishna06112004@gmail.com>
2026-05-01 02:21:19 +0530

    fix: bind to 0.0.0.0 for railway

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'team-task-manager-production-52d7.up.railway.app',
+  baseURL: 'https://team-task-manager-production-52d7.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
