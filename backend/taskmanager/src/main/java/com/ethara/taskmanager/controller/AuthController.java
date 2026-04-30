1c87374ccaa617afef7a6e6f13753bf4ce76ab72
Gopal Krishna <goplakrishna06112004@gmail.com>
2026-04-30 23:58:49 +0530

    updated the code

--- backend/taskmanager/src/main/java/com/ethara/taskmanager/controller/AuthController.java
+++ backend/taskmanager/src/main/java/com/ethara/taskmanager/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "https://team-task-manager-ebon-one.vercel.app")
+@CrossOrigin(origins = "https://team-task-manager-smoky.vercel.app/")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
